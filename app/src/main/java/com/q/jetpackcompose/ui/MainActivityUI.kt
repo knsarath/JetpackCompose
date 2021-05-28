297032ff9fc76b6eb11b1df7ba0dfcc8398b3fc6
sarath <knsarath@live.com>
2021-05-28 23:08:33 +0530

    Surface

--- app/src/main/java/com/q/jetpackcompose/ui/MainActivityUI.kt
+++ app/src/main/java/com/q/jetpackcompose/ui/MainActivityUI.kt
@@ -1,19 +1,13 @@
 package com.q.jetpackcompose.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.q.jetpackcompose.R
 
 
 /**
@@ -21,47 +15,22 @@ import com.q.jetpackcompose.R
  */
 class MainActivityUI {
 
-    @Preview
     @Composable
-    fun DefaultPreview() {
-        NewsStory()
-    }
-
-    @Composable
-    fun NewsStory() {
-        MaterialTheme {
-            val typography = MaterialTheme.typography
-            Column(
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.header),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(180.dp)
-                        .fillMaxWidth()
-                        .clip(shape = RoundedCornerShape(4.dp)),
-                    contentScale = ContentScale.Crop
-                )
-                Spacer(Modifier.height(16.dp))
-
-
-                Text(
-                    "A day wandering through the sandhills " +
-                            "in Shark Fin Cove, and a few of the " +
-                            "sights I saw",
-                    style = typography.h6,
-                            maxLines = 2,
-                    overflow = TextOverflow.Ellipsis)
-
-                Text("Davenport, California",
-                    style = typography.body2)
-                Text("December 2018",
-                    style = typography.body2)
-
-            }
+    fun MyApp() {
+            Surface(color = Color.Yellow) {
+                Greeting(name = "Android")
         }
     }
 
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        MyApp()
+    }
 
 }
