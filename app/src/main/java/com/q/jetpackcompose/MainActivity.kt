3f88c0514e2ccef01a95349f79872e4090dacaf1
sarath <knsarath@live.com>
2021-05-28 23:20:59 +0530

    Button click

--- app/src/main/java/com/q/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/q/jetpackcompose/MainActivity.kt
@@ -3,7 +3,15 @@ package com.q.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.q.jetpackcompose.ui.MainActivityUI
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.q.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,9 +19,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-               MainActivityUI().MyApp()
+                ClickableButton()
             }
         }
+    }
+}
+
+@Composable
+fun ClickableButton() {
+    var clikedTimes = remember { mutableStateOf(0) }
+    Button(onClick = {
+        clikedTimes.value++
+    },  modifier = Modifier.fillMaxWidth()
+        .padding(16.dp)
+        .padding(16.dp)) {
+        Text(text = "Clicked me ${clikedTimes.value} times")
+
     }
 }
 
