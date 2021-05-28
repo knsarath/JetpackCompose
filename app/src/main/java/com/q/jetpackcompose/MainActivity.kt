1f89b24d58300d9f2c3e900d81825e0c10d6c588
sarath <knsarath@live.com>
2021-05-28 23:41:39 +0530

    Button click

--- app/src/main/java/com/q/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/q/jetpackcompose/MainActivity.kt
@@ -3,14 +3,20 @@ package com.q.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.q.jetpackcompose.ui.theme.JetpackComposeTheme
 
@@ -19,23 +25,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                ClickableButton()
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun ClickableButton() {
-    var clikedTimes = remember { mutableStateOf(0) }
-    Button(onClick = {
-        clikedTimes.value++
-    },  modifier = Modifier.fillMaxWidth()
-        .padding(16.dp)
-        .padding(16.dp)) {
-        Text(text = "Clicked me ${clikedTimes.value} times")
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
 
+    Column(modifier = Modifier.fillMaxHeight()) {
+        Column(modifier = Modifier.weight(1f)) {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+        }
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
     }
 }
 
+@Composable
+fun Greeting(name: String) {
+    Surface(Modifier.background(color = Color.Blue)) {
+        Text(text = name, modifier = Modifier.padding(16.dp))
+    }
+}
 
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(
+        onClick = { updateCount(count + 1) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(text = "Counter")
+    }
+}
