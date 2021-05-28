d28f803c726bbbc0dd6a26ff9c12a8d8d8455dd9
sarath <knsarath@live.com>
2021-05-29 00:06:23 +0530

    Animating

--- app/src/main/java/com/q/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/q/jetpackcompose/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.q.jetpackcompose
+
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
-    MaterialTheme {
+    MaterialTheme() {
         Surface(color = Color.Yellow) {
             content()
         }
@@ -37,7 +39,7 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #${it.absoluteValue}" }) {
     val counterState = remember { mutableStateOf(0) }
 
     Column(modifier = Modifier.fillMaxHeight()) {
@@ -63,7 +65,18 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .wrapContentSize(Alignment.CenterStart)
+            .background(color = backgroundColor, )
+            .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Composable
